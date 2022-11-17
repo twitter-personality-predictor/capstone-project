8abe6a7dda95eefbfdc44cf0bbb020945aa039bd
Michael Haerle <michael.haerle01@gmail.com>
2022-11-17 11:22:42 -0600

    added new model functions

--- michael/model.py
+++ michael/model.py
@@ -56,17 +56,16 @@ import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
 
-def get_baseline(df):
-    return f'Baseline Accuracy: {round(df.type.value_counts(normalize=True).max(), 4)*100}'
-
-def top_3_16_personalities(df):
-    # Make the object
-    tfidf = TfidfVectorizer()
-    # Fit/Transform
-    X = tfidf.fit_transform(df.lemmatized)
+def top_3_personality_domains(df):
+    # 60/20/20 Train, Validate, Test split
+    train_val, test = train_test_split(df, stratify=df.type, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_val, stratify=train_val.type, test_size=.25, random_state=123)
+    # Make the object and fit/transform it
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(df.lemmatized)
     # What we are predicting
-    y = df.type
-    # Split X and y into train, validate, and test 
+    y = df.personality_domain
+    # Split X and y into train, validate, and test.
     X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, stratify=y, test_size=.2, random_state=123)
     X_train, X_validate, y_train, y_validate = train_test_split(X_train_val, y_train_val, stratify=y_train_val, test_size=.25, random_state=123)
     # Make train and validate a dataframe
@@ -75,33 +74,26 @@ def top_3_16_personalities(df):
     # Make the object and fit it
     lm = LogisticRegression().fit(X_train, y_train)
     # Make columns for the predictions
-    train['predicted_lm'] = lm.predict(X_train)
-    validate['predicted_lm'] = lm.predict(X_validate)
-    # Make the object and fit it
-    MNBclf = MultinomialNB()
-    MNBclf.fit(X_train, y_train)
-    # Make columns for the predictions
-    train['predicted_MNBclf'] = MNBclf.predict(X_train)
-    validate['predicted_MNBclf'] = MNBclf.predict(X_validate)
-
-    # Make the object and fit/transform it
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(df.lemmatized)
-    # Split X and y into train, validate, and test.
-    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, stratify=y, test_size=.2, random_state=123)
-    X_train, X_validate, y_train, y_validate = train_test_split(X_train_val, y_train_val, stratify=y_train_val, test_size=.25, random_state=123)
-    # Make the object and fit it
-    lm = LogisticRegression().fit(X_train, y_train)
-    # Make columns for the predictions
     train['bow_predicted_lm'] = lm.predict(X_train)
     validate['bow_predicted_lm'] = lm.predict(X_validate)
-    # Make the object and fit it
-    MNBclf = MultinomialNB()
-    MNBclf.fit(X_train, y_train)
-    # Make columns for the predictions
-    train['bow_predicted_MNBclf'] = MNBclf.predict(X_train)
-    validate['bow_predicted_MNBclf'] = MNBclf.predict(X_validate)
 
+    df['personality_domain_encoded'] = df['personality_domain'].apply(filter_3)
+
+    # Make the object
+    tfidf = TfidfVectorizer()
+    # Fit/Transform
+    X = tfidf.fit_transform(df.lemmatized)
+    # What we are predicting
+    y = df.personality_domain_encoded
+    # Split X and y into train, validate, and test 
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, stratify=y, test_size=.2, random_state=123)
+    X_train, X_validate, y_train, y_validate = train_test_split(X_train_val, y_train_val, stratify=y_train_val, test_size=.25, random_state=123)
+
+    xgb_clf = XGBClassifier()
+    xgb_clf.fit(X_train, y_train)
+    score_0 = xgb_clf.score(X_train, y_train)
+    score_1 = xgb_clf.score(X_validate, y_validate)
+    
     # 60/20/20 Train, Validate, Test split
     train_val2, test2 = train_test_split(df, stratify=df.type, test_size=.2, random_state=123)
     train2, validate2 = train_test_split(train_val2, stratify=train_val2.type, test_size=.25, random_state=123)
@@ -111,7 +103,7 @@ def top_3_16_personalities(df):
     # Fit/Transform
     X = tfidf.fit_transform(train2.lemmatized)
 
-    cluster = KMeans(init = 'k-means++', n_clusters=8, n_init=15, random_state=123)
+    cluster = KMeans(init = 'k-means++', n_clusters=4, n_init=15, random_state=123)
     cluster.fit(X)
     yhat = cluster.predict(X)
     train2['cluster'] = cluster.labels_
@@ -121,7 +113,7 @@ def top_3_16_personalities(df):
     # Transform
     X = tfidf.transform(validate2.lemmatized)
 
-    cluster = KMeans(init = 'k-means++', n_clusters=8, n_init=15, random_state=123)
+    cluster = KMeans(init = 'k-means++', n_clusters=4, n_init=15, random_state=123)
     cluster.fit(X)
     yhat = cluster.predict(X)
     validate2['cluster'] = cluster.labels_
@@ -129,14 +121,14 @@ def top_3_16_personalities(df):
     validate2 = pd.concat([validate2, dummies], axis=1)
 
     # Features
-    X_train = train2[['clust_0', 'clust_1', 'clust_2', 'clust_3', 'clust_4', 'clust_5', 'clust_6', 'clust_7']]
+    X_train = train2[['clust_0', 'clust_1', 'clust_2', 'clust_3']]
     # What we are predicting
-    y_train = train2.type
+    y_train = train2.personality_domain
 
     # Features
-    X_validate = validate2[['clust_0', 'clust_1', 'clust_2', 'clust_3', 'clust_4', 'clust_5', 'clust_6', 'clust_7']]
+    X_validate = validate2[['clust_0', 'clust_1', 'clust_2', 'clust_3']]
     # What we are predicting
-    y_validate = validate2.type
+    y_validate = validate2.personality_domain
 
     # Make train and validate a dataframe
     train2 = pd.DataFrame(dict(actual=y_train))
@@ -151,13 +143,13 @@ def top_3_16_personalities(df):
     # Print out the results
     print('Top Model per Feature')
     print('-------------')
-    print('Bag of Words MultinomialNB Train Accuracy: {:.2%}'.format(accuracy_score(train.actual, train.bow_predicted_MNBclf)))
+    print('Bag of Words Logistic Regression Train Accuracy: {:.2%}'.format(accuracy_score(train.actual, train.bow_predicted_lm)))
     print('-------------')
-    print('Bag of Words MultinomialNB Validate Accuracy: {:.2%}'.format(accuracy_score(validate.actual, validate.bow_predicted_MNBclf)))
+    print('Bag of Words Logistic Regression Validate Accuracy: {:.2%}'.format(accuracy_score(validate.actual, validate.bow_predicted_lm)))
     print('-------------')
-    print('TF-IDF MultinomialNB Train Accuracy: {:.2%}'.format(accuracy_score(train.actual, train.predicted_MNBclf)))
+    print('XBG TF-IDF Personality Domain Train Accuracy:', round(score_0 * 100, 2), '%')
     print('-------------')
-    print('TF-IDF MultinomialNB Validate Accuracy: {:.2%}'.format(accuracy_score(validate.actual, validate.predicted_MNBclf)))
+    print('XBG TF-IDF Personality Domain Validate Accuracy:', round(score_1 * 100,2), '%')
     print('-------------')
     print()
     print()
@@ -168,25 +160,20 @@ def top_3_16_personalities(df):
     print('Cluster MultinomialNB Validate Accuracy: {:.2%}'.format(accuracy_score(validate2.actual, validate2.clust_predicted_MNBclf)))
     print('-------------')
 
-def test_16_personalities(df):
+def personality_domain_test(df):
     # Make the object
     tfidf = TfidfVectorizer()
     # Fit/Transform
     X = tfidf.fit_transform(df.lemmatized)
     # What we are predicting
-    y = df.type
+    y = df.personality_domain_encoded
     # Split X and y into train, validate, and test 
     X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, stratify=y, test_size=.2, random_state=123)
     X_train, X_validate, y_train, y_validate = train_test_split(X_train_val, y_train_val, stratify=y_train_val, test_size=.25, random_state=123)
-    # Make test a dataframe
-    test = pd.DataFrame(dict(actual=y_test))
-    # Make the object and fit it
-    MNBclf = MultinomialNB()
-    MNBclf.fit(X_train, y_train)
-    # Make columns for the predictions
-    test['predicted_MNBclf'] = MNBclf.predict(X_test)
 
-    # Print out the results
+    xgb_clf = XGBClassifier()
+    xgb_clf.fit(X_train, y_train)
+    score = xgb_clf.score(X_test, y_test)
     print('-------------')
-    print('TF-IDF MultinomialNB Test Accuracy: {:.2%}'.format(accuracy_score(test.actual, test.predicted_MNBclf)))
+    print('XBG TF-IDF Personality Domain Test Accuracy:', round(score * 100, 2), '%')
     print('-------------')
